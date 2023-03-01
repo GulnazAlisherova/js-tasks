68c1e3cbaedb6bd5cd1a4907a322c0d8ecb66e39
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-03-01 23:12:51 +0600

    formated JS document

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 let oneNumber;
 let twoNumber;
-while (isNaN(oneNumber) || oneNumber=='') {
+while (isNaN(oneNumber) || oneNumber == '') {
   oneNumber = prompt('Введите Первое число: ');
   if (isNaN(oneNumber)) alert(oneNumber + " - это не число");
 }
-while (isNaN(twoNumber) || twoNumber=='') {
+while (isNaN(twoNumber) || twoNumber == '') {
   twoNumber = prompt('Введите Второе число: ');
   if (isNaN(twoNumber)) alert(twoNumber + " - это не число");
 }
- 
+
 alert('Вы ввели числа: ' + oneNumber + ' и ' + twoNumber);
- 
-if (oneNumber > twoNumber){
+
+if (oneNumber > twoNumber) {
   alert('Первое число больше второго');
 }
-if (oneNumber < twoNumber){
+if (oneNumber < twoNumber) {
   alert('Второе число больше первого');
 }
-if(oneNumber === twoNumber){
+if (oneNumber === twoNumber) {
   alert('числа равны');
 }
