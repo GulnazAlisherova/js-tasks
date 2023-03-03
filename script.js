d3e72057a574abb0cdbc9ede11ce705e58b5fd97
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-03-03 20:19:51 +0600

    commented out last example

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // let oneNumber;
-let twoNumber;
+// let twoNumber;
 // while (isNaN(oneNumber) || oneNumber == '') {
 //   oneNumber = prompt('Введите Первое число: ');
 //   if (isNaN(oneNumber)) alert(oneNumber + " - это не число");
@@ -20,20 +20,23 @@ let twoNumber;
 // if (oneNumber === twoNumber) {
 //   alert('числа равны');
 // }
-let oneNumber
 
+// let oneNumber;
+// let twoNumber;
+// while (isNaN(oneNumber) || oneNumber == '') {
+//   oneNumber = prompt('Введите Первое число: ');
+//   if (isNaN(oneNumber)) alert(oneNumber + " - это не число");
+// }
+// while (isNaN(twoNumber) || twoNumber == '') {
+//   if (isNaN(twoNumber)) alert(twoNumber + " - это не число");
+// }
 
-while (isNaN(twoNumber) || twoNumber == '') {
-  oneNumber = prompt('Enter a number');
-};
-
-while (isNaN(twoNumber) || twoNumber == '') {
-  twoNumber = prompt('Введите Второе число: ');
-  if (isNaN(twoNumber)) alert(twoNumber + " - это не число");
-}
-if (oneNumber >= 20 ){
-  alert('Too high');
-}
-else{
-  alert('Thank you');
-}
+// if (oneNumber >= 20) {
+//   alert('Too high');
+// }
+// if (oneNumber < 20) {
+//   alert('Thank you');
+// }
+// if (oneNumber === twoNumber) {
+//   alert('числа равны');
+// }
