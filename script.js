1115ffab0bd38d7c68ffbd3fce262ce961123619
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-03-03 20:10:41 +0600

    commented out first JS example and added new first variable

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-let oneNumber;
-let twoNumber;
-while (isNaN(oneNumber) || oneNumber == '') {
-  oneNumber = prompt('Введите Первое число: ');
-  if (isNaN(oneNumber)) alert(oneNumber + " - это не число");
-}
-while (isNaN(twoNumber) || twoNumber == '') {
-  twoNumber = prompt('Введите Второе число: ');
-  if (isNaN(twoNumber)) alert(twoNumber + " - это не число");
-}
+// let oneNumber;
+// let twoNumber;
+// while (isNaN(oneNumber) || oneNumber == '') {
+//   oneNumber = prompt('Введите Первое число: ');
+//   if (isNaN(oneNumber)) alert(oneNumber + " - это не число");
+// }
+// while (isNaN(twoNumber) || twoNumber == '') {
+//   twoNumber = prompt('Введите Второе число: ');
+//   if (isNaN(twoNumber)) alert(twoNumber + " - это не число");
+// }
 
-alert('Вы ввели числа: ' + oneNumber + ' и ' + twoNumber);
+// alert('Вы ввели числа: ' + oneNumber + ' и ' + twoNumber);
 
-if (oneNumber > twoNumber) {
-  alert('Первое число больше второго');
-}
-if (oneNumber < twoNumber) {
-  alert('Второе число больше первого');
-}
-if (oneNumber === twoNumber) {
-  alert('числа равны');
-}
+// if (oneNumber > twoNumber) {
+//   alert('Первое число больше второго');
+// }
+// if (oneNumber < twoNumber) {
+//   alert('Второе число больше первого');
+// }
+// if (oneNumber === twoNumber) {
+//   alert('числа равны');
+// }
+let oneNumber
